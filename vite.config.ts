95daf68a2e3c8f7467835923d63e96d1814030bf
Botond Nemeth <botond.nemeth@instructure.com>
2026-02-01 22:17:52 +0100

    Update base path in Vite configuration for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./szellozoszuro.hu/",
+  base: "/szellozoszuro.hu/",
   server: {
     host: "::",
     port: 8080,
